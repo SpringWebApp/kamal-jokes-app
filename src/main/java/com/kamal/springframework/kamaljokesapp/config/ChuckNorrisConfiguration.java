2bb5e2c903811658cb0465beb1de8ceba6c57d7e
Kamaldwip Choudhury (V) <kamaldwip_choudhury@apple.com>
2020-04-07 01:59:39 -0500

    Demonstrate Java Spring Configuration

---
+++ src/main/java/com/kamal/springframework/kamaljokesapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,15 @@
+package com.kamal.springframework.kamaljokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+
+}
--- src/main/java/com/kamal/springframework/kamaljokesapp/services/JokeServiceImpl.java
+++ src/main/java/com/kamal/springframework/kamaljokesapp/services/JokeServiceImpl.java
@@ -5,8 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
     @Override
     public String getRandomJoke() {
-        return new ChuckNorrisQuotes().getRandomQuote();
+       return chuckNorrisQuotes.getRandomQuote();
     }
 }
